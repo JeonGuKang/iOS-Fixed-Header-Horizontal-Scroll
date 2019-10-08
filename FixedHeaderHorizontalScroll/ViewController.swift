331fdbd98dcceccb7249ee3dc2640e9136e0049a
jeongu kang <jeongu.kang@okiconcession.com>
2019-10-08 11:35:46 +0900

    Initial Commit

---
+++ FixedHeaderHorizontalScroll/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FixedHeaderHorizontalScroll
+//
+//  Created by 강전구 on 08/10/2019.
+//  Copyright © 2019 강전구. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
